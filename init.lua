e3d2f509decb94c3f19d033735faaa75b4b87970
spcameron <cameron.seanpatrick@gmail.com>
2025-07-21 10:56:42 -0400

    Update black timeout to 5s

--- init.lua
+++ init.lua
@@ -779,7 +779,7 @@ require('lazy').setup({
           return nil
         else
           return {
-            timeout_ms = 500,
+            timeout_ms = 5000,
             lsp_format = 'fallback',
           }
         end
