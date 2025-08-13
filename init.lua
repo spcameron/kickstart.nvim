097d8356e1eb4b9117bc63dabdb9959a0ba906c8
spcameron <cameron.seanpatrick@gmail.com>
2025-08-12 20:48:45 -0400

    Rollback and Fix mason-lspconfig

--- init.lua
+++ init.lua
@@ -677,6 +677,7 @@ require('lazy').setup({
         clangd = {},
         gopls = {},
         pyright = {},
+        ruff = {},
         html = {},
         cssls = {},
         ts_ls = {
@@ -685,17 +686,6 @@ require('lazy').setup({
             documentRangeFormattingProvider = false,
           }),
         },
-
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -728,7 +718,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'isort',
+        'ruff', -- Use to format Python code
         'black', -- Used to format Python code
         'prettier', -- Used to format HTML/CSS/JS/TS
         'clang-format', -- Used to format C
@@ -786,7 +776,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        python = { 'isort', 'black' },
+        python = { 'ruff_fix', 'black' },
         go = { 'gofmt' },
         html = { 'prettier' },
         css = { 'prettier' },
