3b842a884fe823abd2e958b61b5b27a79b5701e5
spcameron <cameron.seanpatrick@gmail.com>
2025-08-15 13:56:24 -0400

    add telescope.lua

---
+++ lua/custom/plugins/telescope.lua
@@ -0,0 +1,75 @@
+-- fuzzy finder (files, lsp, etc.)
+-- see `:help telescope`
+
+return {
+  'nvim-telescope/telescope.nvim',
+  event = 'VimEnter',
+  branch = '0.1.x',
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end,
+    },
+    { 'nvim-telescope/telescope-ui-select.nvim' },
+    { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+  },
+  opts = {
+    pickers = {
+      colorscheme = {
+        enable_preview = true,
+      },
+    },
+    extensions = {
+      ['ui-select'] = {
+        require('telescope.themes').get_dropdown(),
+      },
+    },
+  },
+  config = function()
+    pcall(require('telescope').load_extension, 'fzf')
+    pcall(require('telescope').load_extension, 'ui-select')
+
+    -- see `:help telescope.builtin`
+    local builtin = require 'telescope.builtin'
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    map('n', '<leader>sh', builtin.help_tags, { desc = '[s]earch [h]elp' })
+    map('n', '<leader>sk', builtin.keymaps, { desc = '[s]earch [k]eymaps' })
+    map('n', '<leader>sf', builtin.find_files, { desc = '[s]earch [f]iles' })
+    map('n', '<leader>ss', builtin.builtin, { desc = '[s]earch [s]elect telescope' })
+    map('n', '<leader>sw', builtin.grep_string, { desc = '[s]earch current [w]ord' })
+    map('n', '<leader>sg', builtin.live_grep, { desc = '[s]earch by [g]rep' })
+    map('n', '<leader>sd', builtin.diagnostics, { desc = '[s]earch [d]iagnostics' })
+    map('n', '<leader>sr', builtin.resume, { desc = '[s]earch [r]esume' })
+    map('n', '<leader>s.', builtin.oldfiles, { desc = '[s]earch recent files ("." for repeat)' })
+    map('n', '<leader><leader>', builtin.buffers, { desc = '[ ] find existing buffers' })
+
+    map('n', '<leader>/', function()
+      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+      })
+    end, { desc = '[/] fuzzy search in current buffer' })
+
+    map('n', '<leader>s/', function()
+      builtin.live_grep {
+        grep_open_files = true,
+        prompt_title = 'Live Grep in Open Files',
+      }
+    end, { desc = '[s]earch [/] in open files' })
+
+    map('n', '<leader>sn', function()
+      builtin.find_files { cwd = vim.fn.stdpath 'config' }
+    end, { desc = '[s]earch [n]eovim files' })
+  end,
+}
+
+-- vim: ts=2 sts=2 sw=2 et
